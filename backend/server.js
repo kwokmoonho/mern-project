adf2370b5f6213ad89ae518d69d31ac856d9f579
kwokmoonho <kwokmoonho@gmail.com>
2020-03-19 22:00:05 -0600

    done

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
-mongoose.connect(uri);
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
+// mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
