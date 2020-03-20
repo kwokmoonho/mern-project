132f1ea623222a8f6e6605ea29ae9fbbab02ed90
kwokmoonho <kwokmoonho@gmail.com>
2020-03-19 21:35:01 -0600

    done

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // const uri = process.env.ATLAS_URI;
-mongoose.connect(process.env.ATLAS_URI || 'mongodb://localhost/labhelper');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/labhelper');
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
