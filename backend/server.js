919f0cea482cd4b3247984cd4bb53f0a91f0e0d6
kwokmoonho <kwokmoonho@gmail.com>
2020-03-19 21:22:29 -0600

    done

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = "mongodb+srv://admin:admin@lab-helper-ht9zx.gcp.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
