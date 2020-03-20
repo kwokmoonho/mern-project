d6fa217a01141c64b086b4e066c9c6e7d68ede01
kwokmoonho <kwokmoonho@gmail.com>
2020-03-19 22:05:26 -0600

    done

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 // mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -23,6 +21,8 @@ const usersRouter = require('./routes/users');
 
 app.use('/helps', helpsRouter);
 app.use('/users', usersRouter);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
