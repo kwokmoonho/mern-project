a2ffc711369c0ce8dfb142bd4de826466dcf2287
kwokmoonho <kwokmoonho@gmail.com>
2020-04-01 22:01:19 -0600

    db

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require("path")
-
-require('dotenv').config();
-
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
 const app = express();
+require('./database');
+
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json());
+//API | routes
 const helpsRouter = require('./routes/helps');
 const usersRouter = require('./routes/users');
 app.use('/helps', helpsRouter);
 app.use('/users', usersRouter);
 
-const uri = process.env.ATLAS_URI;
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+
 mongoose.connect(uri || 'mongodb://localhost/labhelper', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
-
-const port = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static( 'client/build' ));
-
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); // relative path
-  });
-}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
