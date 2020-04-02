e85411f1e687cf0283f819cbcf18b9410ddbab93
kwokmoonho <kwokmoonho@gmail.com>
2020-04-01 21:34:12 -0600

    db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require("path")
 
 require('dotenv').config();
 
@@ -22,6 +23,15 @@ app.use('/helps', helpsRouter);
 app.use('/users', usersRouter);
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static( 'client/build' ));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); // relative path
+  });
+}
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
