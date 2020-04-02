cf2e1b532910d54cefe77bdb4e1c09bd7c019018
kwokmoonho <kwokmoonho@gmail.com>
2020-04-01 22:41:04 -0600

    db

--- src/components/create-help.component.js
+++ src/components/create-help.component.js
@@ -18,8 +18,9 @@ export default class CreateHelp extends Component {
     }
   }
 
+  //without locathost
   componentDidMount() {
-    axios.get('http://localhost:5000/users/')
+    axios.get('users')
       .then(response => {
         if (response.data.length > 0) {
           this.setState({
@@ -64,7 +65,7 @@ export default class CreateHelp extends Component {
 
     console.log(help);
 
-    axios.post('http://localhost:5000/helps/add', help)
+    axios.post('helps/add', help)
       .then(res => console.log(res.data));
 
     window.location = '/';
--- src/components/helps-list.component.js
+++ src/components/helps-list.component.js
@@ -23,7 +23,7 @@ export default class HelpsList extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/helps/')
+    axios.get('helps/')
       .then(response => {
         this.setState({ helps: response.data })
       })
@@ -33,7 +33,7 @@ export default class HelpsList extends Component {
   }
 
   deleteHelp(id) {
-    axios.delete('http://localhost:5000/helps/'+id)
+    axios.delete('helps/'+id)
       .then(response => { console.log(response.data)});
 
     this.setState({
