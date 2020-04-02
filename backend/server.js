fa19f6ffdb7aaee838c734d37fd607e0dc410c3a
kwokmoonho <kwokmoonho@gmail.com>
2020-04-01 21:36:18 -0600

    db

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const helpsRouter = require('./routes/helps');
+const usersRouter = require('./routes/users');
+app.use('/helps', helpsRouter);
+app.use('/users', usersRouter);
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri || 'mongodb://localhost/labhelper', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
@@ -15,12 +19,6 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
-
-const helpsRouter = require('./routes/helps');
-const usersRouter = require('./routes/users');
-
-app.use('/helps', helpsRouter);
-app.use('/users', usersRouter);
 
 const port = process.env.PORT || 5000;
 
@@ -31,7 +29,6 @@ if (process.env.NODE_ENV === 'production') {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); // relative path
   });
 }
-
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
