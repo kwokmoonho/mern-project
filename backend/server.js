79c101394ad8dcce75f5a33bc598714e633945ea
kwokmoonho <kwokmoonho@gmail.com>
2020-04-03 00:19:39 -0600

    db

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,34 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
+const helpsRouter = require('./routes/helps');
+const usersRouter = require('./routes/users');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
+const port = process.env.PORT || 8080;
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected!!!!');
+});
 
-const helpsRouter = require('./routes/helps');
-const usersRouter = require('./routes/users');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.use(morgan('tiny'));
 app.use('/helps', helpsRouter);
 app.use('/users', usersRouter);
 
