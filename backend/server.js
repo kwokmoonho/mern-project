d9d995f40ee76b6b4e83dedd7372d29f36eff50b
kwokmoonho <kwokmoonho@gmail.com>
2020-04-03 00:55:44 -0600

    db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path')
 
 require('dotenv').config();
 
@@ -11,7 +9,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
@@ -19,11 +16,6 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedT
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
-})
-
-app.use(express.static(path.join(__dirname, '../public/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/build'))
 })
 
 const helpsRouter = require('./routes/helps');
